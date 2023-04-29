d4a4dc147f2a57cae5e40b9e2c26db5940fb1287
reaganmunene <reagannmunene@gmail.com>
2023-04-29 17:54:26 +0300

    updated js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,58 @@
-fetch('https://raw.githubusercontent.com/reaganmunene/phase-1-indepent-project/main/src/db.json')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+const form = document.querySelector('form');
+const errorDiv = document.querySelector('.error');
+const errorText = document.querySelector('.error-text');
+const input = document.querySelector('input');
 
-  async function getData() {
-    try {
-      const response = await fetch('https://raw.githubusercontent.com/reaganmunene/phase-1-indepent-project/main/src/db.json');
-      const data = await response.json();
-  
-      // Loop through the destinations array and display each city and its hotels
-      data.destinations.forEach(destination => {
-        const city = destination.city;
-        const hotels = destination.hotels;
-  
-        // Create an HTML element for the city and append it to the body
-        const cityEl = document.createElement('h2');
-        cityEl.textContent = city;
-        document.body.appendChild(cityEl);
-  
-        // Loop through the hotels array and display each hotel's name, rating, and image
-        hotels.forEach(hotel => {
-          const name = hotel.name;
-          const rating = hotel.rating;
-          const img = hotel.img;
-  
-          // Create an HTML element for the hotel and append it to the body
-          const hotelEl = document.createElement('div');
-          hotelEl.innerHTML = `
-            <h3>${name}</h3>
-            <p>Rating: ${rating}</p>
-            <img src="${img}" alt="${name}">
-          `;
-          document.body.appendChild(hotelEl);
-        });
-      });
-    } catch (error) {
-      console.error(error);
-    }
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const ipaddress = input.value;
+
+  if (ipaddress.trim() === '') {
+    displayError('Enter a valid IP address');
+    return;
   }
-  
-  getData();
+
+  console.log('first');
+  httpHandler(false);
+});
+
+function renderMap() {
+  var map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  var marker = L.marker([51.5, -0.09]).addTo(map);
+  var circle = L.circle([51.5, -0.09], {
+    color: '#f87171',
+    fillColor: '#fca5a5',
+    fillOpacity: 0.5,
+    radius: 500,
+  }).addTo(map);
+}
+
+function displayError(message) {
+  errorDiv.classList.add('show');
+  errorText.textContent = message;
+
+  setTimeout(() => {
+    errorDiv.classList.remove('show');
+  }, 3000);
+}
+
+async function httpHandler(isSearch, ipaddress = '') {
+  let url = '';
+  if (isSearch) {
+    url = `https://api.ipbase.com/v2/info?ip=${ipaddress}&apikey=TvFfKg0Xj2lparECBfEZ5pjwrU7yMu6RoafBHohv`;
+  } else {
+    url =
+      'https://api.ipbase.com/v2/info?apikey=TvFfKg0Xj2lparECBfEZ5pjwrU7yMu6RoafBHohv';
+  }
+
+  const response = await fetch(url);
+  const { data } = await response.json();
+  return data;
+}
+
+renderMap();
